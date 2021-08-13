ae9314b0381e9c38b83a9ada4a3b6b12419fce0a
Andy O'Brien <andrew.obrien@tgfoe.org>
2021-08-13 11:10:00 -0700

    set default verbosity for udp_bridge to debug

--- src/udp_bridge_node.cpp
+++ src/udp_bridge_node.cpp
@@ -1,9 +1,15 @@
 #include "udp_bridge/udpbridge.h"
+#include <ros/console.h>
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "udp_bridge_node");
 
+    
+    if( ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug) ) {
+        ros::console::notifyLoggerLevelsChanged();
+    }
+    
     udp_bridge::UDPBridge bridge;
     
     bridge.spin();
