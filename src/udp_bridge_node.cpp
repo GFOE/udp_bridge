e845a587b300af802ca310f1df253e03baf32bc4
GFOE <gfoe@tgfoe.org>
2021-08-15 18:57:53 +0000

    disable debug

--- src/udp_bridge_node.cpp
+++ src/udp_bridge_node.cpp
@@ -6,9 +6,9 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "udp_bridge_node");
 
     
-    if( ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug) ) {
-        ros::console::notifyLoggerLevelsChanged();
-    }
+    //if( ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug) ) {
+    //    ros::console::notifyLoggerLevelsChanged();
+    //}
     
     udp_bridge::UDPBridge bridge;
     
